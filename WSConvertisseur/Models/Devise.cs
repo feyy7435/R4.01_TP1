2cd2b1b8c3c3996d9e6d1ad5f46c3a8279e98ef0
tinastef <tinastef@A275-S28>
2025-01-21 15:02:34 +0100

    Ajout du model devise

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,37 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int nomdevise;
+
+        public int NomDevise
+        {
+            get { return nomdevise; }
+            set { nomdevise = value; }
+        }
+
+        private int taux;
+
+        public int Taux
+        {
+            get { return taux; }
+            set { taux = value; }
+        }
+        public Devise(){ }
+
+        public Devise(int id, int nomdevise, int taux)
+        {
+            this.id = id;
+            this.nomdevise = nomdevise;
+            this.taux = taux;
+        }
+    }
+}
