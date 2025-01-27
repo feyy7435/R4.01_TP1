aa41a5d98b44b1c9b3987ca91e29c9b1e087ab3e
Feyza Tinastepe <Feyza.Tinastepe@etu.univ-smb.fr>
2025-01-27 10:11:54 +0100

    controller

--- WSConvertisseur/Controllers/DeviseController.cs
+++ WSConvertisseur/Controllers/DeviseController.cs
@@ -7,54 +7,102 @@ namespace WSConvertisseur.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DeviseController : ControllerBase
+    public class DevisesController : ControllerBase
     {
-        private readonly List<Devise> devises;
+        private List<Devise> devises;
 
-        public DeviseController()
+        public DevisesController()
         {
-            devises = new List<Devise>
-        {
-            new Devise(1, "Dollar", 1.08),
-            new Devise(2, "Franc Suisse", 1.07),
-            new Devise(3, "Yen", 120.0)
-        };
+            devises = new List<Devise>();
+            devises.Add(new Devise(1, "Dollar", 1.08));
+            devises.Add(new Devise(2, "Franc Suisse", 1.07));
+            devises.Add(new Devise(3, "Yen", 120));
         }
-        // GET: api/<DeviseController>
+
+        // GET: api/<DevisesController>
         [HttpGet]
+        [ProducesResponseType(200)]
+        //public IEnumerable<string> Get()
         public IEnumerable<Devise> GetAll()
         {
+            //return new string[] { "value1", "value2" };
             return devises;
         }
 
-        // GET api/<DeviseController>/5
+        /// <summary>
+        /// Get a single currency.
+        /// </summary>
+        /// <returns>Http response</returns>
+        /// <param name="id">The id of the currency</param>
+        /// <response code="200">When the currency id is found</response> 
+        /// <response code="404">When the currency id is not found</response> 
+        //public string Get(int id)
+        //public Devise GetById(int id)
+        // GET api/<DevisesController>/5
         [HttpGet("{id}", Name = "GetDevise")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
         public ActionResult<Devise> GetById(int id)
         {
-            Devise? devise = devises.FirstOrDefault(d => d.Id == id);
+            Devise? devise = devises.FirstOrDefault((d) => d.Id == id);
             if (devise == null)
             {
                 return NotFound();
             }
-            return (devise);
+            return devise;
         }
 
-        // POST api/<DeviseController>
+        // POST api/<DevisesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        //public void Post([FromBody] string value)
+        public ActionResult<Devise> Post([FromBody] Devise devise)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            devises.Add(devise);
+            return CreatedAtRoute("GetDevise", new { id = devise.Id }, devise);
+            //OU: return CreatedAtAction("GetById", new { id = devise.Id }, devise);
+
         }
 
-        // PUT api/<DeviseController>/5
+        // PUT api/<DevisesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        //public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] Devise devise)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != devise.Id)
+            {
+                return BadRequest();
+            }
+            int index = devises.FindIndex((d) => d.Id == id);
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            devises[index] = devise;
+            return NoContent();
         }
 
-        // DELETE api/<DeviseController>/5
+        // DELETE api/<DevisesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        //public void Delete(int id)
+        public ActionResult<Devise> Delete(int id)
         {
+            Devise? devise = devises.FirstOrDefault((d) => d.Id == id);
+            if (devise == null)
+            {
+                return NotFound();
+            }
+            devises.Remove(devise);
+            return devise;
         }
     }
 }
